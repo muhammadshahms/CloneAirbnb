548cc90518edf8c4c30be97dde8ceb159fc28a47
salahshakeel <99357263+salahshakeel@users.noreply.github.com>
2023-11-20 01:27:25 +0500

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,9 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
+import Layout from "./Layout";
 import IndexPage from "./pages/indexPage";
 import LoginPage from "./pages/loginPage";
-import Layout from "./Layout";
+import RegisterPage from "./pages/registerPage";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Route path="/" element={<Layout />}>
         <Route index element={<IndexPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Route>
     </Routes>
   );
--- client/src/pages/loginPage.jsx
+++ client/src/pages/loginPage.jsx
@@ -1,13 +1,21 @@
+import { Link } from "react-router-dom"
+
 const LoginPage = () => {
   return (
 
-    <div className="mt-4">
+    <div className="mt-4 grow flex items-center justify-around ">
+      <div className="mb-64">
       <h1 className="text-4xl text-center"> Login</h1>
       <form className="max-w-md mx-auto mt-4">
         <input type="email" placeholder={'youremail@example.com'}/>
         <input type="password" placeholder={'password'}/>
-        <button>Login</button>
+        <button className="primary">Login</button>
+        <div className="text-center py-2 text-gray-500">
+          
+           <Link to={'/register'}> Don't have an account?</Link>
+        </div>
       </form>
+    </div>
     </div>
   )
 }
